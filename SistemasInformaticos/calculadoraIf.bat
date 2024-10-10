92fa3bb5ee7cbdaa0f7474cad360671a18a1ec13
AmadorFiol <amfibo24@gmail.com>
2024-10-10 21:34:26 +0200

    Cambiar nombre de calculadora a calculadoraGoTo y creacion calculadoraIf

--- SistemasInformaticos/calculadoraIf.bat
+++ SistemasInformaticos/calculadoraIf.bat
@@ -1,5 +1,7 @@
 @echo off
-:calc
+REM V.If
+cls REM Limpiar la pantalla
+
 echo "Calculadora"
 echo "S:Sumar"
 echo "R:Resta"
@@ -8,36 +10,37 @@ echo "D:Division"
 echo "Q:Salir"
 
 set /p input="Que quieres hacer: "
-goto %input%
 
-:s
+REM RECUERDA EL PUTO ESPACIO
+if "%input%"=="S" (
     set /p x="Di un primer num: "
+    echo "%x%"
     set /p y="Di un segundo num: "
+    echo "%y%"
     set /a z=x+y
     echo "El resultado de la suma de %x% y %y% es %z%"
     pause
-    goto calc
-:r
+)else if "%input%"=="R" (
     set /p x="Di un primer num: "
     set /p y="Di un segundo num: "
     set /a z=x-y
     echo "El resultado de la resta de %x% y %y% es %z%"
     pause
-    goto calc
-:m
+)else if "%input%"=="M" (
     set /p x="Di un primer num: "
     set /p y="Di un segundo num: "
     set /a z=x*y
-    echo "El resultado de la multiplicacion de %x% y %y% es %z%
+    echo "El resultado de la multiplicacion de %x% y %y% es %z%"
     pause
-    goto calc
-:d
+)else if "%input%"=="D" (
     set /p x="Di un primer num: "
     set /p y="Di un segundo num: "
-    set /a z=x/y
-    echo "El resultado de la division de %x% y %y% es %z%"
-    pause
-    goto calc
-
-:q
-    exit
+    if "%y%"=="0" (
+        echo "No se puede dividir entre 0"
+        )else (
+            set /a z=x/y
+            echo "El resultado de la division de %x% y %y% es %z%"
+        )
+        pause
+)
+REM <LSS, >GTR, =< LEQ, >= GEQ
