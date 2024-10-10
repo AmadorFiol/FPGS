d8d14adda985304f14ff3f1f21e4663db9c9c0c4
AmadorFiol <amfibo24@gmail.com>
2024-10-10 19:57:56 +0200

    Añado contenido del examen de la UD1

---
+++ Programacion/ExamenUD1/Ej1.py
@@ -0,0 +1,6 @@
+'''
+Amador Fiol Borel
+Examen UD1
+10/10/2024
+'''
+nums=[15,29,34,45,67,88,93]
---
+++ Programacion/ExamenUD1/Ej2.py
@@ -0,0 +1,7 @@
+def ejercicio2(cantidad):
+    print("Has pedido ",cantidad," de hamburgesas")
+    valor_calculado=cantidad*4.75
+    print("El coste de tu pedido es: ",valor_calculado)
+
+cantidad=int(input("Cuantas hamburgesas quieres pedir? "))
+ejercicio2(cantidad)
