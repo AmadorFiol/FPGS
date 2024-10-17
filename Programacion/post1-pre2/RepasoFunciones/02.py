fd5839ab2cae14be5d2e391c6fecfa7d6f0ad053
AmadorFiol <amfibo24@gmail.com>
2024-10-17 15:47:29 +0200

    Reorganizacion directorios

--- Programacion/post1-pre2/RepasoFunciones/02.py
+++ Programacion/post1-pre2/RepasoFunciones/02.py
@@ -1,10 +1,6 @@
 #Escribir una función que reciba una lista con 2 números y devuelva un diccionario con el mayor, menor, media
 def orderNum(parametro):
-    if(parametro[0]>parametro[1]):
-        diccionario={"mayor":parametro[0],"menor":parametro[1],"media":(parametro[0]+parametro[1])/len(parametro)}
-    else:
-        diccionario={"mayor":parametro[1],"menor":parametro[0],"media":(parametro[0]+parametro[1])/len(parametro)}
-
+    diccionario={"mayor":max(parametro),"menor":min(parametro),"media":sum(parametro)/len(parametro)}
     print("El numero mas grande es: ",diccionario["mayor"],"el numero menor es: ",diccionario["menor"],"y la media es: ",diccionario["media"])
 
 array=[45,25]
