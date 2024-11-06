75f5a701375e2a2a60f7e9a19482384f057b3562
AmadorFiol <amfibo24@gmail.com>
2024-11-06 11:02:06 +0100

    Creado carpeta y movido archivos para reorganizar

---
+++ Programacion/U2/Condicionales/Act01.py
@@ -0,0 +1,6 @@
+#Crea script en python que pida un num y diga si es > a 10
+num=int(input("Dime un num: "))
+if(num>10):
+    print("El numero es mayor a 10")
+else:
+    print("El numero es menor a 10")
---
+++ Programacion/U2/Condicionales/Act03.py
@@ -0,0 +1,6 @@
+#Crea script que pida un num y te diga si es un numero par
+num=int(input("Dime un numero: "))
+if(num%2==0):
+    print("El numero es par")
+else:
+    print("El numero es inpar")
