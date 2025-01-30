c695daa30b0f254ea81eb6adfb1e75d364b6e106
AmadorFiol <amfibo24@gmail.com>
2025-01-30 20:09:18 +0100

    Cambiados if's por case's

--- SistemasInformaticos/bash/calc.sh
+++ SistemasInformaticos/bash/calc.sh
@@ -3,36 +3,37 @@ echo R. Restar
 echo M. Multiplicar
 echo D. Dividir
 read -p "" input
-if [ $input = "S" ]
-then
+case $input in
+	"S")
 	read -p "" x
 	read -p "" y
 	let resultado=$x+$y
 	echo "El resultado de $x + $y es $resultado"
-fi
-if [ $input = "R" ]
-then
+	;;
+	"R")
 	read -p "" x
-	read -p "" y
-	let resultado=$x-$y
-	echo "El resultado de $x - $y es $resultado"
-fi
-if [ $input = "M" ]
-then
+        read -p "" y
+        let resultado=$x-$y
+        echo "El resultado de $x - $y es $resultado"
+	;;
+	"M")
 	read -p "" x
-	read -p "" y
-	let resultado=$x*$y
-	echo "El resultado de $x * $y es $resultado"
-fi
-if [ $input = "D" ]
-then
+        read -p "" y
+        let resultado=$x*$y
+        echo "El resultado de $x * $y es $resultado"
+	;;
+	"D")
 	read -p "" x
-	read -p "" y
-	if [ $y = 0 ]
-	then
-		echo "No se puede dividir entre 0"
-	else
-		let resultado=$x/$y
-		echo "El resultado de $x / $y es $resultado"
-	fi
-fi
+        read -p "" y
+        if [ $y = 0 ]
+        then
+                echo "No se puede dividir entre 0"
+        else
+                let resultado=$x/$y
+                echo "El resultado de $x / $y es $resultado"
+        fi
+	;;
+	*)
+	echo "Esa no es una opcion valida"
+	;;
+esac
