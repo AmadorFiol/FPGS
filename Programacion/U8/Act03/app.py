4e8545d1f47ba6e3f7a4bfe4b7344efdb0d7d76c
AmadorFiol <amfibo24@gmail.com>
2025-05-08 11:55:12 +0200

    Finalizado act03

--- Programacion/U8/Act03/app.py
+++ Programacion/U8/Act03/app.py
@@ -3,7 +3,26 @@ def mover(ext,file):
     os.system(f"move {file} {ext}/{file}")
     log.write(f"El archivo {file} se ha movido a {ext}\n")
 
+def check_dir():
+    dir=os.path.basename(os.getcwd())
+    if dir != "Act03":
+        notSecure=True
+        while(notSecure):
+            print(f"Estas ejecutando el programa en el directorio {dir}")
+            match(input("Estas seguro de continuar (Y/N) ").upper()):
+                case "Y" | "S":
+                    notSecure=False
+                case "N":
+                    exit()
+                case _:
+                    print("Esa no es una opcion correcta, porfavor intente denuevo")
+                    os.system("pause")
+                    os.system("cls")
+        os.system("cls")
+
+
 log=open("log.txt","a")
+check_dir()
 dir=os.listdir("./")
 print(dir)
 for file in dir:
@@ -17,9 +36,16 @@ for file in dir:
             log.write(f"Se ha creado la carpeta {ext}\n")
             mover(ext,file)
         else:
-            mover(ext,file)
-    except IndexError:
+            if file in os.listdir(f"./{ext}"):
+                raise FileExistsError
+            else:
+                mover(ext,file)
+    except IndexError: #Este error salta cuando se encuentra una carpeta
         pass
+    except FileExistsError:
+        print(f"El archivo ya existe en la carpeta {file.split(".")[1]}")
+    except PermissionError:
+        print("No tienes lo permisos correspondientes, se pasara al siguiente archivo")
 log.close()
 dir=os.listdir("./")
 print(dir)
