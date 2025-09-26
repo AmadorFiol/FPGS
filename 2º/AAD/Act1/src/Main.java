701afbafc6e1b33699512114279a291a02f2f120
Amador Fiol <amfibo24@gmail.com>
2025-09-26 17:48:56 +0200

    Continuo act ficheros

--- 2º/AAD/Act1/src/Main.java
+++ 2º/AAD/Act1/src/Main.java
@@ -1,9 +1,9 @@
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
 //Escribir 100 palabras, luego encontrar la mas buscada
 //Encontrar y controlar las posibles excepciones: FileNotFound, IOException, Permisos
 
@@ -13,23 +13,28 @@ public class Main {
         try {
             file.createNewFile();
             FileWriter fileWriter = new FileWriter(file.getName());
-            FileReader fileReader = new FileReader(file.getName());
             String[] words = new String[5];
             words[0]="Hola";
             words[1]="Chao";
             words[2]="Miau";
             words[3]="Guau";
             words[4]="Dunno";
-            Random r = new Random();
+            Random random = new Random();
             for(int i=0;i<100;i++){
-                fileWriter.write(words[r.nextInt(5-0)]);
+                fileWriter.write(words[random.nextInt(0,4)]+"\n");
             }
+
+            fileWriter.close();
             HashMap<String, Integer> wordCount = new HashMap<>();
-            for(String word:fileReader.readAllLines()){
-                if(wordCount.containsKey(word)){
-                    wordCount.put(word, wordCount.get(word)+1);
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()){
+                System.out.println(fileReader.nextLine());
+                if(wordCount.containsKey(fileReader.nextLine())){
+                    if (fileReader.nextLine()!=null){
+                        wordCount.put(fileReader.nextLine(), wordCount.get(fileReader.nextLine())+1);
+                    }
                 } else {
-                    wordCount.put(word,1);
+                    wordCount.put(fileReader.nextLine(),1);
                 }
             }
 
